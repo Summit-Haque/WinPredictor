1be5135c323755e11009852843200bba9df17f89
Summit Haque <summit.haque@gmail.com>
2016-06-04 12:02:28 +0600

    Add files via upload

--- yamlFileReader.py
+++ yamlFileReader.py
@@ -33,6 +33,9 @@ for num in range(211028,211029):
 
                 # player_of_match = data['info']['player_of_match'][0]
                 # city = data['info']['city']
+                umpires = data['info']['umpires']
+                dates = data['info']['dates'][0]
+                venue = data['info']['venue']
                 teams = ' vs '.join(data['info']['teams'])
                 team_one = data['info']['teams'][0]
                 team_two = data['info']['teams'][1]
@@ -51,20 +54,11 @@ for num in range(211028,211029):
                 ''' This part for forming data for 1st Innings of a match
                     require ball, team, batsman, bowler, runs, team run,
                 '''
-                ball_list, batsman_run_list, team_total_list, total_run_list, batsman_list, bowler_list, \
-                innings_list, wicket_list, player_out_list, ballfaced_striker_list, strike_rate_list  = [
-                    [], [], [], [], [], [], [], [], [], [], []]
-
-                player_name, ball_faced, run_taken = [[], [], []]
+                ball_list, batsman_run_list, team_total_list, total_run_list,player_out_list, wicket_list, striker_list, non_striker_list, bowler_list, innings_list, striker_run_list, \
+                non_striker_run_list = [[], [], [], [], [], [], [], [], [], [], [], []]
 
                 team_one_total = 0
                 wicket = 0
-                ballfaced_striker = 0
-                ballfaced_non_striker = 0
-                striker = ''
-                non_striker = ''
-                striker_run = 0
-                non_striker_run = 0
 
                 for k in firstInningsDeliveryData:
 
@@ -74,95 +68,47 @@ for num in range(211028,211029):
                     batsman_run = k[ball]['runs']['batsman']
                     total_run = k[ball]['runs']['total']
                     batsman_name = k[ball]['batsman']
+                    non_striker = k[ball]['non_striker']
 
-                    non_striker_name = k[ball]['non_striker']
-
-                    bowler_name = k[ball]['bowler']
-
-                    flag = 0
-                    i = 0
-                    print(i, len(player_name))
-                    for i in player_name:
-                        print(batsman_name,i,player_name[i])
-                        if batsman_name == player_name[i]:
-                            try:
-                                wide = k[ball]['extras']['wides']
-                            except:
-                                ball_faced[i] = ball_faced[i] + 1
-                        ballfaced_striker = ball_faced[i]
-                        run_taken[i] = run_taken[i] + batsman_run
-                        striker_run = run_taken[i]
-                        flag = 1
-
-                    if flag == 0:
-                        #length = len(player_name)
-                        #print(length)
-                        player_name.append(batsman_name)
-                        ball_faced.append(0)
-                        run_taken.append(0)
-                        ballfaced_striker = 0
-                        striker_run = 0
-                        print(player_name[i], ball_faced[i], run_taken)
-
-                    #print(striker, non_striker, ballfaced_striker, ballfaced_non_striker, striker_run, non_striker_run)
-                    #if batsman_name != striker :
-                        #if batsman_name == non_striker:
-                            #striker, non_striker = non_striker, striker
-                            #ballfaced_striker, ballfaced_non_striker = ballfaced_non_striker, ballfaced_striker
-                            #striker_run, non_striker_run = non_striker_run, striker_run
-                        #else:
-                            #striker = batsman_name
-                            #non_striker = non_striker_name
-                            #ballfaced_striker = 0
-                            #striker_run = 0
-
-                   #ballfaced_striker = ballfaced_striker + 1
-                    #striker_run = striker_run + batsman_run
-
-
-
-
+                    striker_run = 0
+                    non_striker_run = 0
+                    for old_k in firstInningsDeliveryData:
+                        old_ball = list(old_k.keys())[0]
+                        if old_k[old_ball]['batsman'] == batsman_name:
+                            striker_run = striker_run + old_k[old_ball]['runs']['batsman']
+                        elif old_k[old_ball]['batsman'] == non_striker:
+                            non_striker_run = non_striker_run + old_k[old_ball]['runs']['batsman']
+                        if ball == old_ball:
+                            break
 
                     try:
                         player_out = k[ball]['wicket']['player_out']
                         wicket = wicket + 1
 
-                        # if player_out == striker:
-                        # striker_run = 0
-                        # ballfaced_striker = 0
-                        # else:
-                        # non_striker_run = 0
-                        # ballfaced_non_striker = 0
                     except:
                         player_out = ''
 
+                    bowler_name = k[ball]['bowler']
                     # print(batsman_run, " ", total_run)
                     team_one_total += total_run
                     # Construct List for pandas Series
                     innings_list.append('1st')
                     ball_list.append(ball)
-                    batsman_list.append(batsman_name)
+                    striker_list.append(batsman_name)
+                    non_striker_list.append(non_striker)  # new
                     batsman_run_list.append(batsman_run)
                     bowler_list.append(bowler_name)
                     team_total_list.append(team_one_total)
                     total_run_list.append(total_run)
+                    striker_run_list.append(striker_run)  # new
+                    non_striker_run_list.append(non_striker_run)  # new
                     player_out_list.append(player_out)
                     wicket_list.append(wicket)
-                    ballfaced_striker_list.append(ballfaced_striker)
-                    try:
-                        ballfaced_striker != 0
-                        strike_rate_list.append(striker_run/ballfaced_striker * 100)
-                    except:
-                        strike_rate_list.append(0)
+
+
 
                 team_two_total = 0
                 wicket = 0
-                ballfaced_striker = 0
-                ballfaced_non_striker = 0
-                striker = ''
-                non_striker = ''
-                striker_run = 0
-                non_striker_run = 0
 
                 for k in secondInningsDeliveryData:
                     # print(list(k.keys())[0])
@@ -171,96 +117,68 @@ for num in range(211028,211029):
                     batsman_run = k[ball]['runs']['batsman']
                     total_run = k[ball]['runs']['total']
                     batsman_name = k[ball]['batsman']
+
+                    non_striker = k[ball]['non_striker']  # new
+
+                    striker_run = 0
+                    non_striker_run = 0
+                    for old_k in firstInningsDeliveryData:
+                        old_ball = list(old_k.keys())[0]
+                        if old_k[old_ball]['batsman'] == batsman_name:
+                            striker_run = striker_run + old_k[old_ball]['runs']['batsman']
+                        elif old_k[old_ball]['batsman'] == non_striker:
+                            non_striker_run = non_striker_run + old_k[old_ball]['runs']['batsman']
+                        if ball == old_ball:
+                            break
+
                     bowler_name = k[ball]['bowler']
-
-                    flag = 0
-                    i = 0
-                    for i in player_name:
-                        if batsman_name == player_name[i]:
-                            try:
-                                wide = k[ball]['extras']['wides']
-                            except:
-                                ball_faced[i] = ball_faced[i] + 1
-                        ballfaced_striker = ball_faced[i]
-                        run_taken[i] = run_taken[i] + batsman_run
-                        striker_run = run_taken[i]
-                        flag = 1
-
-                        # if flag == 0:
-                        # length = len(player_name)
-                        # print(length)
-                        # player_name[i] = batsman_name
-                        # ball_faced[i] = 0
-                        # run_taken = 0
-                        # ballfaced_striker = 0
-                        # striker_run = 0
-
-                        # print(striker, non_striker, ballfaced_striker, ballfaced_non_striker, striker_run, non_striker_run)
-                        # if batsman_name != striker :
-                        # if batsman_name == non_striker:
-                        # striker, non_striker = non_striker, striker
-                        # ballfaced_striker, ballfaced_non_striker = ballfaced_non_striker, ballfaced_striker
-                        # striker_run, non_striker_run = non_striker_run, striker_run
-                        # else:
-                        # striker = batsman_name
-                        # non_striker = non_striker_name
-                        # ballfaced_striker = 0
-                        # striker_run = 0
-
-                        # ballfaced_striker = ballfaced_striker + 1
-                        # striker_run = striker_run + batsman_run
-
 
                     try:
                         player_out = k[ball]['wicket']['player_out']
                         wicket = wicket + 1
 
-                        #if player_out == striker:
-                            #striker_run = 0
-                            #ballfaced_striker = 0
-                        #else:
-                            #non_striker_run = 0
-                            #ballfaced_non_striker = 0
                     except:
                         player_out = ''
 
+                    bowler_name = k[ball]['bowler']
                     team_two_total += total_run
                     # Construct List for pandas Series
                     innings_list.append('2nd')
                     ball_list.append(ball)
-                    batsman_list.append(batsman_name)
+                    striker_list.append(batsman_name)
+                    non_striker_list.append(non_striker)  # new
                     batsman_run_list.append(batsman_run)
                     bowler_list.append(bowler_name)
                     team_total_list.append(team_two_total)
                     total_run_list.append(total_run)
-                    player_out_list.append(player_out)
+                    striker_run_list.append(striker_run)  # new
+                    non_striker_run_list.append(non_striker_run)  # new
+                    # print(dates, team_one, team_two, toss, venue, umpires)
                     wicket_list.append(wicket)
-                    ballfaced_striker_list.append(ballfaced_striker)
-                    try:
-                        ballfaced_striker != 0
-                        strike_rate_list.append(striker_run / ballfaced_striker * 100)
-                    except:
-                        strike_rate_list.append(0)
+
 
                 # print(dates, team_one, team_two, toss, venue, umpires)
-
 
                 # Construction of a DataFrame
                 data_set = pd.DataFrame({
                     'Team 1': team_one,
                     'Team 2': team_two,
+                    'Winner/Result': result, #new
+                    'Date': str(dates), #new
                     'Toss': toss,
                     'Ball': ball_list,
                     'Venue': venue,
                     'Innings': pd.Series(innings_list),
-                    'Batsman': pd.Series(batsman_list),
+                    'Striker': pd.Series(striker_list),
+                    'Non Striker': pd.Series(non_striker_list), #new
+                    'Striker Run': pd.Series(striker_run_list), #new
+                    'Non Striker Run': pd.Series(non_striker_run_list), #new
                     'Bowler': pd.Series(bowler_list),
                     'Batsman Run': pd.Series(batsman_run_list),
                     'Team Total': pd.Series(team_total_list),
                     'Wicket': pd.Series(wicket_list),
                     'Player Out': pd.Series(player_out_list),
-                    'Ball Faced By Striker': pd.Series(ballfaced_striker_list),
-                    'Strike Rate': pd.Series(strike_rate_list)
+
                 })
 
         value = value+1
